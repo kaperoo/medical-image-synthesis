4de8c62b5443cf3abae5eb70f55fb7b25440dbc0
silvrhawk <er-2@hotmail.co.uk>
2025-02-12 19:26:23 +0000

    added FID distance for multi-class synthesised images

--- utils/FID_result.py
+++ utils/FID_result.py
@@ -1,8 +1,9 @@
 import torch
 import torchvision.datasets
 import torchvision.transforms as transforms
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Subset
 from torchvision import datasets, transforms
+import os
 
 import matplotlib.pyplot as plt
 
@@ -10,23 +11,26 @@ BATCH_SIZE = 8
 
 FAKE_INPUT_PATH = "fake_data"
 REAL_INPUT_PATH = "augmented_data"
+CATEGORISED = False
 
 transform = transforms.Compose([
     transforms.Grayscale(num_output_channels=3),
     transforms.ToTensor(),
 ])
 
-# IMAGES MUST BE PREPROCESSED BEFOREHAND. MAKE SURE ALL IMAGES MATCH IN DIMENSIONS
-# AND ENSURE THAT THE NO. OF IMAGES ARE THE SAME IN BOTH FOLDERS
-real_dataset = datasets.ImageFolder(REAL_INPUT_PATH, transform=transform)
-fake_dataset = datasets.ImageFolder(FAKE_INPUT_PATH, transform=transform)
+def create_dataloaders(dataset, batch_size=16):
+    class_dataloaders = {}
+    idx_to_class = {v: k for k, v in dataset.class_to_idx.items()}
+    class_indices = {class_name: [] for class_name in dataset.classes}
+    for idx, (_, class_idx) in enumerate(dataset.imgs):
+        class_name = idx_to_class[class_idx]
+        class_indices[class_name].append(idx)
 
-## LOADING DATA AND COMPARE FOR ALL CLASSES
-# TODO: INTRODUCE SEPARATE DATA LOADERS FOR EACH CLASS
+    for class_name, indices in class_indices.items():
+        subset = Subset(real_dataset, indices)  # Create subset for each class
+        class_dataloaders[class_name] = DataLoader(subset, batch_size=batch_size, shuffle=True)
 
-fake_dataloader = DataLoader(fake_dataset, batch_size = BATCH_SIZE, shuffle = True, num_workers = 4)
-
-real_dataloader = DataLoader(real_dataset, batch_size = BATCH_SIZE, shuffle = True, num_workers = 4)
+    return class_dataloaders
 
 ## Below code refers to the original implementation of FID score calculation
 # Implemented with the help of: https://minibatchai.com/2022/07/23/FID.html
@@ -69,13 +73,36 @@ def frechet_inception_distance(m_w, C_w, m, C, debug=False):
     fid = ((m - m_w)**2).sum() + C.trace() + C_w.trace() - 2 * trace_sqrt_CCw
     return fid
 
+# IMAGES MUST BE PREPROCESSED BEFOREHAND. MAKE SURE ALL IMAGES MATCH IN DIMENSIONS
+# AND ENSURE THAT THE NO. OF IMAGES ARE THE SAME IN BOTH FOLDERS
+
+## LOADING DATA AND COMPARE ACCORDING TO CLASSES (CLASSIFIED DATA)
+real_dataset = datasets.ImageFolder(REAL_INPUT_PATH, transform=transform)
+fake_dataset = datasets.ImageFolder(FAKE_INPUT_PATH, transform=transform)
+
+if CATEGORISED:
+    real_class_dataloader = create_dataloaders(real_dataset, batch_size=16)
+    fake_class_dataloader = create_dataloaders(fake_dataset, batch_size=16)
+
+## LOADING DATA AND COMBINING ALL IMAGES INTO ONE CLASS (UNCLASSIFIED DATA)
+else:
+    real_class_dataloader = DataLoader(fake_dataset, batch_size = BATCH_SIZE, shuffle = True, num_workers = 4)
+    fake_class_dataloader = DataLoader(real_dataset, batch_size = BATCH_SIZE, shuffle = True, num_workers = 4)
+
 if __name__ == '__main__':
     model = get_model()
 
-    m_real_data, C_real_data = get_moments(real_dataloader, model)
-    m_fake_data, C_fake_data = get_moments(fake_dataloader, model)
+    if CATEGORISED:
+        for key in real_class_dataloader:
+            print(key)
+            m_real_data, C_real_data = get_moments(real_class_dataloader[key], model)
+            m_fake_data, C_fake_data = get_moments(fake_class_dataloader[key], model)
 
-    fid = frechet_inception_distance(m_real_data, C_real_data, m_fake_data, C_fake_data, debug=False)
-    
-    # The lower the score, the closer the generated images are to the real images
-    print(fid)
+            fid = frechet_inception_distance(m_real_data, C_real_data, m_fake_data, C_fake_data, debug=False)
+            print(-fid)
+    else:
+        m_real_data, C_real_data = get_moments(real_class_dataloader, model)
+        m_fake_data, C_fake_data = get_moments(fake_class_dataloader, model)
+
+        fid = frechet_inception_distance(m_real_data, C_real_data, m_fake_data, C_fake_data, debug=False)
+        print(-fid)
